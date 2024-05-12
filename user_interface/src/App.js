06b0b9434e857165efb2ddcffafd52ef653eba08
JhondhelPago <jhondhelpago2307@gmail.com>
2024-05-12 15:06:11 +0800

    user_interface development file

---
+++ user_interface/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-6xl text-red-500'>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
