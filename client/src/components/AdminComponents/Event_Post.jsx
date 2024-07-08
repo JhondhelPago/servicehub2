2477dbc031944f69c81598873859bcbb68901c20
bibe1s <jrsolis213@gmail.com>
2024-07-08 23:48:52 +0800

    remove-Approve-Remove-Button

--- client/src/components/AdminComponents/Event_Post.jsx
+++ client/src/components/AdminComponents/Event_Post.jsx
@@ -135,11 +135,6 @@ const PostInfoDiv = (props) => {
                         {/* <!-- buttons --> */}
                         <div className="grid gap-5 text-white grid-flow-dense lg:grid-flow-col">
 
-                            <button className="flex items-center justify-between w-4/5 gap-2 p-3 mx-auto bg-green-600 rounded-lg lg:mx-0 lg:w-auto scaleHover">
-                                Approve
-                                <svg className="h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m5 12l5 5L20 7" /></svg>
-                            </button>
-
                             <button className="flex items-center justify-between w-4/5 gap-2 p-3 mx-auto bg-orange-600 rounded-lg lg:mx-0 lg:w-auto scaleHover">
                                 Archive
                                 <svg className="h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M224 48H32a16 16 0 0 0-16 16v24a16 16 0 0 0 16 16v88a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16v-88a16 16 0 0 0 16-16V64a16 16 0 0 0-16-16m-16 144H48v-88h160Zm16-104H32V64h192zM96 136a8 8 0 0 1 8-8h48a8 8 0 0 1 0 16h-48a8 8 0 0 1-8-8" /></svg>
@@ -151,11 +146,6 @@ const PostInfoDiv = (props) => {
                                 <svg className="h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fillRule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="currentColor" d="M12.707 15.707a1 1 0 0 1-1.414 0L5.636 10.05A1 1 0 1 1 7.05 8.636l4.95 4.95l4.95-4.95a1 1 0 0 1 1.414 1.414z" /></g></svg>
                                 {/* <!-- hide/arrow up icon - when stats are shown --> */}
                                 <svg className="hidden h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" fillRule="evenodd"><path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" /><path fill="currentColor" d="M11.293 8.293a1 1 0 0 1 1.414 0l5.657 5.657a1 1 0 0 1-1.414 1.414L12 10.414l-4.95 4.95a1 1 0 0 1-1.414-1.414z" /></g></svg>
-                            </button>
-
-                            <button className="flex items-center justify-between w-4/5 gap-2 p-3 mx-auto bg-red-600 rounded-lg lg:mx-0 lg:w-auto scaleHover" onClick={() => { removeFnc(props.data) }}>
-                                Remove
-                                <svg className="h-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6zM8 9h8v10H8zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" /></svg>
                             </button>
 
                             <button className="flex items-center justify-between w-4/5 gap-2 p-3 mx-auto rounded-lg lg:mx-0 lg:w-auto scaleHover bg-primary-light" onClick={() => { ParentFunc(props.data) }}>
