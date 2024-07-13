d776a2679a07ad3deda7c8917f82e1c212da0c92
JhondhelPago <jhondhelpago2307@gmail.com>
2024-07-13 16:15:51 +0800

    CloseButton Activated

--- client/src/components/HomeComponents/inbox_component.jsx
+++ client/src/components/HomeComponents/inbox_component.jsx
@@ -209,9 +209,12 @@ const MailOverView = ({ MailObjsArray, ContactAdminId, CloseMailOverViewAction, 
 
     useEffect(() => {
 
+        //call the XButton_FalseState here if there is a problem
+
         if (Overflow_InnerInbox.current) {
             Overflow_InnerInbox.current.scrollTop = Overflow_InnerInbox.current.scrollHeight;
         }
+
 
     }, [])
 
@@ -226,6 +229,17 @@ const MailOverView = ({ MailObjsArray, ContactAdminId, CloseMailOverViewAction, 
 
     const ReplyDeactivate = () => {
         SetReplyButtonState(false);
+    }
+
+
+    const [XButtonBoolState, setXButtonBoolState] = useState(false);
+
+    const XButton_TrueState = () =>{
+        setXButtonBoolState(true);
+    }
+
+    const XButton_FalseState = () => {
+        setXButtonBoolState(false);
     }
 
     return (
@@ -244,7 +258,7 @@ const MailOverView = ({ MailObjsArray, ContactAdminId, CloseMailOverViewAction, 
                         {/* <p className="">Subj</p> */}
 
                         {/* close btn */}
-                        <button className="hover:text-red-600">
+                        <button className="hover:text-red-600" onClick={CloseMailOverViewAction}>
                             <svg className="h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12L5.7 16.89a.996.996 0 1 0 1.41 1.41L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4" /></svg>
                         </button>
                     </div>
